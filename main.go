99032fe478839b6167672dc92b0e474ebebb86be
Martin Nacimiento <martinnacimiento98@gmail.com>
2020-04-23 04:10:58 -0300

    alto commit

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = ":8000"
+		port = "8000"
 	}
 	server := NewServer(":" + port)
 	server.handle("/confessions", "GET", controllers.IndexConfession)
